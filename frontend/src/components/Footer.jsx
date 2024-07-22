fe37c51fb64bb676bc8a7154357d9b6b83c85319
Abhimanyu004 <120742019+Abhimanyu004@users.noreply.github.com>
2024-07-22 08:39:09 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <div className="footer " style={{'padding-left':'.75rem', 'padding-right':'.75rem'}}>
             <div className="row p-2" style={{ backgroundColor: 'rgb(49,49,49)', color: 'white'}}>
                 <div className="col-6 d-flex justify-content-start">
-                    <h4 className="mb-0 pt-2" style={{ fontSize: '1.2rem' }}>Made By: Abhimanyu | Narenm | Bharath</h4>
+                    <h4 className="mb-0 pt-2" style={{ fontSize: '1.2rem' }}>Made By: Abhimanyu | Naren | Bharath</h4>
                 </div>
                 <div className="col-6 d-flex justify-content-end">
                     <h4><span style={{ fontSize: '1.2rem' }}>Powered by:</span> <a href="https://www.youdata.ai/home" className="btn btn-warning">youdata.ai</a></h4>
